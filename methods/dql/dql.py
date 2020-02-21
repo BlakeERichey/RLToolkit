2ca09f48a5de747690695875fca476e3b53516a6
Blake <blake.e.richey@gmail.com>
2020-02-21 12:22:14 -0600

    Refactored code. Working on Deep Q Learning Method.

---
+++ methods/dql/dql.py
@@ -0,0 +1,11 @@
+from utils import ReplayBuffer
+
+class DQL:
+
+  """
+    DQL is a class the encapsulates the Deep Q Learning Method for 
+    Reinforcement Learning.
+  """
+
+  def __init__(self,):
+    pass
---
+++ utils/ReplayBuffer.py
@@ -0,0 +1,41 @@
+import random
+import numpy as np
+
+class ReplayBuffer:
+
+  """
+    Class dedicated to managing a replay buffer for RL methods.
+
+    Creates a replay buffer of determined size and enables adding experiences
+    to the buffer and taking random samples.
+  """
+
+  def __init__(self, max_size=100):
+    """
+      Initializes an empty ReplayBuffer with a max size of `max_size`.
+    """
+
+    self.memory = []
+    self.max_size = max_size
+  
+  def remember(self,experience):
+    """
+      Stores an experience into replay buffer and removes older memories
+      if resulting ReplayBuffer excedes max buffer size.
+
+      For DQL an example experiences would be:
+      experience = [prev_envstate, action, reward, envstate, done]
+    """
+
+    self.memory.append(experience)
+    
+    if len(self.memory) > self.max_size:
+      del self.memory[0]
+  
+  def get_batch(self,batch_size=1):
+    """
+      Grabs `batch_size` random elements from ReplayBuffer and returns
+      them as a numpy array.
+    """
+
+    return np.array(random.sample(self.memory, batch_size))
