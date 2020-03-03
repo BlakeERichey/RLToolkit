07ac5ff64d2a5ed37f4af99dcd26b8309cd7d0b4
Blake Richey <BRichey@patriots.uttyler.edu>
2020-03-03 10:37:02 -0600

    Added pyramid creation utility and assign workers utility.

--- rltoolkit/methods/experimental/coga/evo.py
+++ rltoolkit/methods/experimental/coga/evo.py
@@ -1,35 +1,36 @@
-from seeker import Seeker
+import random
 import numpy as np
+from colony import Colony
+from worker import Worker
 
 class Evolution:
 
-  def __init__(self,):
-    self.seekers = []
-    self.configured = False
+  def __init__(self, nn, num_colonies=50, num_workers=75, alpha=0.01):
+    self.colonies = [Colony(nn) for _ in range(num_colonies)]
+    self.workers  = [Worker(nn, alpha) for _ in range(num_workers)]
+    self.pyramid =  self._create_pyramid()
 
-  def create_species(self, worker):
-    assert, self.configured == True, "Evolution params not configured. Run Evolution.config()."
-
-    self.seekers.append(Seeker(worker.genes, self.patience))
-    for _ in range(self.num_seekers-1):
-      self.seekers.append(Seeker(worker.clone().genes, self.patience))
-
-  def train(self, env, return_worker=False):
-    assert len(self.seekers), "No Species Created."
-    for gen in range(self.generations):
+  def train(self, env, generations, 
+            elites=None, sharpness=1, goal=None, 
+            patience=25, return_worker=False):
+    assert len(self.colonies), "No Colony Created."
+    for gen in range(generations):
       ranked = []
-      for i, worker in enumerate(self.seekers):
-        res, val = worker.fitness(env, self.sharpness, validate, render)
+      for i, colony in enumerate(self.colonies):
+        res, val = colony.fitness(
+          env=env,
+          sharpness=sharpness, 
+          validate=validate, 
+          render=render,
+        )
         ranked.append((i, res, val))
 
-      ranked = sorted(ranked, key= lambda x: (x[1], x[2]), reverse=True)
+      ranked = sorted(ranked, key=lambda x: (x[1], x[2]), reverse=True)
       print("Gen:", gen, "Ranked:", ranked, '\n')
-      if self.goal:
-        if self.metric == 'reward':
-          goal_met = ranked[0][1]>=self.goal
-        else:
-          goal_met = ranked[0][2]>=self.goal and ranked[0][1]>=self.goal
+      if goal:
+        goal_met = ranked[0][1]>=goal
       
+      #WRONG
       #next gen
       if gen != self.generations - 1 and not(goal_met):
         #Gen new weights
@@ -37,18 +38,18 @@ class Evolution:
         new_weights = []
         for i, worker in enumerate(mating_pool):
           if len(new_weights) < self.pop_size - self.elites:
-            parent1 = self.seekers[worker[0]]
-            parent2 = self.seekers[mating_pool[-i][0]]
+            parent1 = self.colonies[worker[0]]
+            parent2 = self.colonies[mating_pool[-i][0]]
             weights = parent1.breed(parent2)
             new_weights.append(weights)
         
         #determine if new mask is needed
         for i, tup in enumerate(ranked):
           worker_id = tup[0]
-          self.seekers[worker_id].add_rank(i, self.pop_size)
+          self.colonies[worker_id].add_rank(i, self.pop_size)
 
         #apply new weights and mutate
-        for i, worker in enumerate(self.seekers):
+        for i, worker in enumerate(self.colonies):
           if i > self.elites:
             worker.net.weights = new_weights[i-self.elites]
           worker.mutate()
@@ -57,13 +58,13 @@ class Evolution:
         break
         
     best_worker = ranked[0][0] #id
-    worker = self.seekers[best_worker]
+    worker = self.colonies[best_worker]
     if return_worker:
       return worker
     return worker.net
-
   
   def selection(self, ranked):
+    #WRONG
     mating_pool = []
     for i in range(self.elites):
       mating_pool.append(ranked[i])
@@ -75,29 +76,29 @@ class Evolution:
         mating_pool.append(worker)
 
     return mating_pool
+
+  def _create_pyramid(self,):
+    pyramid = [1 for _ in range(len(self.colonies))]
+
+    #generate pyramid [1,1,1,1,2,3,4,5,6,7]
+    i = 0
+    workers_remaining=len(self.workers)-len(self.colonies)
+    while workers_remaining>0:
+      if i+1<workers_remaining:
+        self.pyramid[i] += i+1
+        workers_remaining -= i+1
+      else:
+        self.pyramid[i]+=workers_remaining
+        workers_remaining=0
+      i+=1
+
+    pyramid = sorted(pyramid, reverse=True)
+    return pyramid
   
-  def config(self, 
-    generations, 
-    num_seekers, 
-    elites,
-    num_workers,
-    patience=25,
-    sharpness=1, 
-    goal=None, 
-    metric='reward',):
-
-    self.goal        = goal
-    self.metric      = metric
-    self.elites      = elites
-    self.pop_size    = pop_size
-    self.patience    = patience
-    self.sharpness   = sharpness
-    self.generations = generations
-    self.num_seekers = num_seekers
-    self.num_workers = num_workers
-
-    assert self.elites <= self.pop_size
-    assert self.sharpness > 0
-    assert self.num_workers >= self.num_seekers
-    
-    self.configured = True
+  def _assign_workers(self,):
+    random.shuffle(self.workers)
+    assigned = 0
+    for i, colony in enumerate(self.colonies):
+      assigning = self.pyramid[i]
+      colony.workers = self.workers[assigned:assigning]
+      assigned+=assigning
